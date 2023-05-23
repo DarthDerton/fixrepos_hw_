7a99794b2f127e0ff98a36a8d67e114ef34214f0
Killa~Selai <105960906+DarthDerton@users.noreply.github.com>
2023-05-23 20:56:42 +1000

    /
    /

--- tests/test_hw2.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from hw2 import supressor, supressor_generator
-
-
-@pytest.mark.parametrize("suppressor_type", [supressor, supressor_generator])
-def test_suppressor(suppressor_type):
-    with suppressor_type(IndexError):
-        assert [][2] is None
